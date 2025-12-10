4f734f5963e04b61026f14d280e858993310ecb1
Sonzzini <paulo.sonzzini@gmail.com>
2025-12-10 19:40:11 -0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
