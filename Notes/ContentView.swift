9d200a3c0b1cdc9bcdc5e9f64552994c4bddf50b
Sonzzini <paulo.sonzzini@gmail.com>
2025-12-17 18:57:37 -0300

    MVC

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -6,19 +6,75 @@
 //
 
 import SwiftUI
+import Combine
+import CoreData
+
+
+struct Note: Identifiable {
+	let id = UUID()
+	let title: String
+	let body: String
+	let date: Date
+}
+
+
+class ContentViewController: ObservableObject {
+	
+	let container: NSPersistentContainer
+	
+	var viewContext: NSManagedObjectContext {
+		return container.viewContext
+	}
+	
+//	let dataController
+	
+	@Published var notes: [Note] = [
+		Note(title: "Boa noite!", body: "Este é o corpo da nota de boa noite.", date: Date.now),
+		Note(title: "Aula de persistência", body: "Veremos CoreData pela primeira vez", date: Date.now),
+		Note(title: "Terceira Nota", body: "Corpo da terceira nota", date: Date.now)
+	]
+
+	
+	// CRUD
+	
+	// Create
+	func addNewNote(note: Note) {
+		self.notes.append(note)
+	}
+	
+	func deleteNote() {
+		
+	}
+	
+	func fetchAllNotes() {
+		
+	}
+	
+	func editNote() {
+		
+	}
+	
+}
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	@StateObject var controller = ContentViewController()
+	
+	var body: some View {
+		
+		ForEach(controller.notes) { note in
+			Text("\(note.title)")
+		}
+		
+		Button("Adicionar nota") {
+			let newNote = Note(title: "Nova nota!", body: "Corpo da nova nota", date: Date.now)
+			controller.addNewNote(note: newNote)
+		}
+		
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
